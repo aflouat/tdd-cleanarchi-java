9f9b1e976e81a10f606e278fac13618a6b7ea7e2
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2024-08-06 09:58:07 +0200

    start Palindromes extractor kata

---
+++ src/main/java/com/wealcome/nextride/kata/PalindromesExtractor.java
@@ -0,0 +1,16 @@
+package com.wealcome.nextride.kata;
+
+
+// "Anna est partie faire du Kayak mais flashée par un radar"
+// ["ANNA", "KAYAK", "RADAR"]
+
+
+import java.util.List;
+
+public class PalindromesExtractor {
+
+    public List<String> extract(String sentence) {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/test/java/unit/kata/PalindromesExtractorTest.java
@@ -0,0 +1,14 @@
+package unit.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PalindromesExtractorTest {
+
+    @Test
+    public void doNothing() {
+        assertThat(true).isTrue();
+    }
+
+}
